48092b62c83ea9aa0871f31a141ad4c3e187171a
23p31A0534 <147791788+23p31A0534@users.noreply.github.com>
2023-10-14 22:05:00 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(b*1+c*2>=a)     {         printf("Qualify ");     }     else     {         printf("Not Qualify");     } }

---
+++ Contest:.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(b*1+c*2>=a)
+    {
+        printf("Qualify
+");
+    }
+    else 
+    {
+        printf("Not Qualify");
+    }
+}
