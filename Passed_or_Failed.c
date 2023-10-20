55fa7ef38a48227eb906ee3b4e5aadd172d951e3
23p31A0534 <147791788+23p31A0534@users.noreply.github.com>
2023-10-20 10:30:12 +0530

    #include<stdio.h> int main() {     int a,b,c,d,e,f;     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);     if(a>34 && b>34 && c>34 && d>34 && e>34)     {         printf("PASSED");     }     else     {         printf("FAILED");     } }

---
+++ Passed_or_Failed.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    if(a>34 && b>34 && c>34 && d>34 && e>34)
+    {
+        printf("PASSED");
+    }
+    else
+    {
+        printf("FAILED");
+    }
+}
