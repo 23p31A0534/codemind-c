b189d3fa672ff0f78a615d4272bcfd5e86dd3ce1
23p31A0534 <147791788+23p31A0534@users.noreply.github.com>
2023-11-07 21:25:55 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         if(n%2==0)         {             printf("%d ",2*i);         }         else         {             printf("%d ",2*i);         }     } }

---
+++ First_N_even_Natural_Numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        if(n%2==0)
+        {
+            printf("%d ",2*i);
+        }
+        else 
+        {
+            printf("%d ",2*i);
+        }
+    }
+}
