910f4e92967af22d029b55a2cde8112400bfbf13
23p31A0534 <147791788+23p31A0534@users.noreply.github.com>
2023-12-21 11:59:38 +0530

    #include<stdio.h> int main() {     int i,n,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=0;j<i;j++)         {             printf("* ");         }         printf(" ");     } }

---
+++ Basic_Pattern_2.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+            printf("* ");
+        }
+        printf("
+");
+    }
+}
