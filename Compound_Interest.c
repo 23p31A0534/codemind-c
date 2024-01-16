962a61a3a90e27e33c0fb1b9cde90f523949115c
23p31A0534 <147791788+23p31A0534@users.noreply.github.com>
2024-01-16 19:31:41 +0530

    #include<stdio.h> #include<math.h> int main() {     int P,R,T;     scanf("%d%d%d",&P,&R,&T);     float CI;     CI=P*pow((1+R/100.0),T)-P;     printf("%.2f",CI);
    }

--- Compound_Interest.c
+++ Compound_Interest.c
@@ -4,7 +4,8 @@ int main()
 {
     int P,R,T;
     scanf("%d%d%d",&P,&R,&T);
-    float ci;
-    ci=P*pow((1+R/100.0),T)-P;
-    printf("%.2f",ci);
+    float CI;
+    CI=P*pow((1+R/100.0),T)-P;
+    printf("%.2f",CI);
+    
 }
