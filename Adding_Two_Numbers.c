139459570ff28714ee754190bed83e5680e70719
23p31A0534 <147791788+23p31A0534@users.noreply.github.com>
2024-01-18 22:44:57 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b);     c=a+b;     printf("%d",c); }

--- Adding_Two_Numbers.c
+++ Adding_Two_Numbers.c
@@ -1,15 +1,8 @@
 #include<stdio.h>
-int add(int,int);
 int main()
 {
-    int a,b;
+    int a,b,c;
     scanf("%d%d",&a,&b);
-    int res=add( a, b);
-    printf("%d",res);
-}
-int add(int a,int b)
-{
-    int c;
-     c=a+b;
-    return c;
+    c=a+b;
+    printf("%d",c);
 }
