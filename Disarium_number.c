8c458ee276311e60e732018028d9757fadcca629
23p31A0534 <147791788+23p31A0534@users.noreply.github.com>
2024-01-19 16:05:10 +0530

    #include<stdio.h> #include<math.h> int main() {     int num,rem,sum=0,temp,c=1,i,p=1;     scanf("%d",&num);     temp=num;     while(temp!=0)     {         rem=temp%10;         sum=sum*10+rem;         temp=temp/10;     }     temp=sum;     sum=0;     while(temp!=0)     {         rem=temp%10;         for(i=1;i<=c;i++)         {             p=p*rem;         }         sum=sum+p;         temp=temp/10;         c++;         p=1;     }     if(sum==num)     printf("True");     else     printf("False"); }

---
+++ Disarium_number.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int num,rem,sum=0,temp,c=1,i,p=1;
+    scanf("%d",&num);
+    temp=num;
+    while(temp!=0)
+    {
+        rem=temp%10;
+        sum=sum*10+rem;
+        temp=temp/10;
+    }
+    temp=sum;
+    sum=0;
+    while(temp!=0)
+    {
+        rem=temp%10;
+        for(i=1;i<=c;i++)
+        {
+            p=p*rem;
+        }
+        sum=sum+p;
+        temp=temp/10;
+        c++;
+        p=1;
+    }
+    if(sum==num)
+    printf("True");
+    else
+    printf("False");
+}
